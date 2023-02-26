70b9f6895c8de2b6950f3658497388e5441e4006
Alicia Parra <alicia@redparra.com>
2023-02-26 17:48:24 +0100

    (SearchInput) :sparkles: Add search-input atom component

---
+++ src/atoms/search-input/README.md
@@ -0,0 +1,22 @@
+# music-podcast-app
+
+## Search input
+
+- Search input allow users to input custom text to filter results on a list
+
+### Example
+
+```tsx
+const handleFilterList = (searchTerm) => {
+  console.log(searchTerm)
+}
+
+<SearchInput
+  filterData={handleFilterList}
+  placeholder="Filter elements..."
+/>
+```
+
+## Author
+
+With ❤️ by [Alicia Parra](https://github.com/aliparra)
---
+++ src/atoms/search-input/SearchInput.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { SearchInputProps } from '@/atoms/search-input/search-input.types';
+
+const SearchInput = ({ filterData, placeholder }: SearchInputProps) => {
+	const [searchTerm, setSearchTerm] = useState('');
+
+	/**
+	 * The handleChange function takes an event as an argument, and then sets the searchTerm state to the
+	 * value of the event target, and then calls the filterData function (which will be executed on the parent) with the value of the event
+	 * target.
+	 * @param event - React.ChangeEvent<HTMLInputElement>
+	 */
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchTerm(event.target.value);
+		filterData(event.target.value);
+	};
+
+	return (
+		<div>
+			<input
+				className="form-control p-2"
+				type="text"
+				placeholder={placeholder}
+				value={searchTerm}
+				onChange={handleChange}
+			/>
+		</div>
+	);
+};
+
+SearchInput.defaultProps = {
+	placeholder: 'search...',
+};
+
+SearchInput.propTypes = {
+	placeholder: PropTypes.string,
+	filterData: PropTypes.func.isRequired,
+};
+
+export default SearchInput;
