32f5d439bb1e1932446879f752e1c0335cb4c02f
Alicia Parra <alicia@redparra.com>
2023-02-26 17:39:50 +0100

    (Card) :lipstick: Modify card classes to avoid boostrap overrides

--- src/atoms/card/Card.tsx
+++ src/atoms/card/Card.tsx
@@ -5,11 +5,11 @@ import './card.css';
 const Card = ({ image, title, author }: CardPropTypes) => {
 	return (
 		<div className="card__wrapper">
-			<div className="card">
+			<div className="custom-card">
 				<div className="circle-container">
 					<img className="circle" src={image.url} alt={image.description} />
 				</div>
-				<div className="card-info">
+				<div className="custom-card-info">
 					<h2>{title}</h2>
 					<p>Author: {author}</p>
 				</div>
