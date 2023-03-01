baeb3628d0d936fcbe6d66ad13f3c5788c87f187
Alicia Parra <alicia@redparra.com>
2023-03-01 18:37:00 +0100

    (PodcastPage) :recycle: Simplify podcastPage logic

--- src/routes/podcast-page/PodcastPage.tsx
+++ src/routes/podcast-page/PodcastPage.tsx
@@ -8,22 +8,16 @@ import './podcast-page.css';
 
 function PodcastPage(): JSX.Element {
 	const { podcastId } = useParams<{ podcastId: string }>();
-	const podcastsUrl = `https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json`;
-	const episodesUrl = `https://itunes.apple.com/search?term=${localStorage.getItem(
-		'podcastTitle'
-	)}&entity=podcastEpisode`;
+	const podcastStringData = localStorage.getItem('podcastData');
+	const podcastData = JSON.parse(`${podcastStringData}`);
+
+	const episodesUrl = `https://itunes.apple.com/search?term=${podcastData.title}&entity=podcastEpisode`;
 
 	const {
 		data: episodeData,
 		isLoading: isLoadingEpisodes,
 		hasError: hasErrorEpisodes,
 	} = useFetch(episodesUrl);
-
-	const {
-		data: podcastData,
-		isLoading: isLoadingPodcast,
-		hasError: hasErrorPodcast,
-	} = useFetch(podcastsUrl);
 
 	const tableData = episodeData?.results.map((episode: any) => {
 		return {
@@ -36,46 +30,23 @@ function PodcastPage(): JSX.Element {
 		};
 	});
 
-	const findCurrentPodcast = (id: string) => {
-		const podcastsList = podcastData?.feed?.entry;
-		if (podcastsList) {
-			return podcastsList.find(
-				(podcast: any) => podcast?.id?.attributes['im:id'] === id
-			);
-		}
-	};
-
-	const createCardData = () => {
-		const currentPodcast = podcastId && findCurrentPodcast(podcastId);
-		const cardData = {
-			image: {
-				url: currentPodcast['im:image'][2].label,
-				description: currentPodcast?.title?.label,
-			},
-			title: currentPodcast.title.label,
-			author: currentPodcast['im:artist'].label,
-			description: currentPodcast?.summary?.label || 'No description available',
-		};
-		return cardData;
-	};
-
 	return (
 		<div className="podcast-page">
-			{isLoadingPodcast ? (
-				<div className="spinner-grow text-info" role="status">
-					<span className="visually-hidden">Loading...</span>
-				</div>
-			) : (
+			{podcastData ? (
 				<div className="podcast-page__card">
 					<DescriptionCard
-						title={createCardData().title}
-						author={createCardData().author}
-						description={createCardData().description}
+						title={podcastData.title}
+						author={podcastData.author}
+						description={podcastData.description}
 						image={{
-							url: createCardData().image.url,
-							description: createCardData().image.description,
+							url: podcastData.imageUrl,
+							description: podcastData.imageLabel,
 						}}
 					/>
+				</div>
+			) : (
+				<div className="spinner-grow text-info" role="status">
+					<span className="visually-hidden">Loading...</span>
 				</div>
 			)}
 
